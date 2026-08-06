0eff62d0f87e54dba2e9cf4a5cd784c65dd0f3d2
Konstantin Sychugov <sichugov@tvema.ru>
2026-08-06 14:09:56 +0300

    fix dos2unix for largefiles

--- plugins/dos2unix/__init__.py
+++ plugins/dos2unix/__init__.py
@@ -8,6 +8,12 @@ class Filter():
     def file_data_filter(self,file_data):
         if file_data['file_ctx'] == None:
             return
+        if file_data.get('is_largefile'):
+            # file_ctx here refers to the .hglf/ pointer file, not the real
+            # content, hence file_ctx.isbinary() will not indicate if file is
+            # binary or not. So, let's reimplement it inplace:
+            if b'\0' in file_data['data']:
+                return
         file_ctx = file_data['file_ctx']
         if not file_ctx.isbinary():
             file_data['data'] = file_data['data'].replace(b'\r\n', b'\n')
